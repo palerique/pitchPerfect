190f61a3e27ae44bf2290875a764abc3a269028c
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2018-01-29 21:01:18 -0200

    Fix issues found during review

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -10,37 +10,36 @@ import UIKit
 import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-    
+
     @IBOutlet weak var slowButton: UIButton!
     @IBOutlet weak var fastButton: UIButton!
     @IBOutlet weak var highPitchButton: UIButton!
     @IBOutlet weak var lowPitchButton: UIButton!
     @IBOutlet weak var echoButton: UIButton!
     @IBOutlet weak var reverbButton: UIButton!
-    
+
     @IBOutlet weak var stopButton: UIButton!
-    
+
     var recordedAudioURL: URL!
-    
-    var audioFile:AVAudioFile!
-    var audioEngine:AVAudioEngine!
+
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
     var audioPlayerNode: AVAudioPlayerNode!
     var stopTimer: Timer!
-    
-    enum ButtonType: Int { case slow = 0, fast, chipmunk, vader, echo, reverb }
+
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        slowButton.imageView!.contentMode = UIViewContentMode.scaleAspectFit
-        fastButton.imageView!.contentMode = UIViewContentMode.scaleAspectFit
-        highPitchButton.imageView!.contentMode = UIViewContentMode.scaleAspectFit
-        lowPitchButton.imageView!.contentMode = UIViewContentMode.scaleAspectFit
-        echoButton.imageView!.contentMode = UIViewContentMode.scaleAspectFit
-        reverbButton.imageView!.contentMode = UIViewContentMode.scaleAspectFit
-        
-        stopButton.imageView!.contentMode = UIViewContentMode.scaleAspectFit
-        
+        let arrayOfButtons = [slowButton, fastButton, highPitchButton, lowPitchButton, echoButton, reverbButton, stopButton]
+
+        for button in arrayOfButtons {
+            button?.imageView?.contentMode = .scaleAspectFit
+        }
+
         setupAudio()
     }
 
@@ -48,10 +47,10 @@ class PlaySoundsViewController: UIViewController {
         super.viewWillAppear(animated)
         configureUI(.notPlaying)
     }
-    
+
     @IBAction func playSoundForButton(_ sender: UIButton) {
         print("Play Sound Button Pressed")
-        switch(ButtonType(rawValue: sender.tag)!) {
+        switch (ButtonType(rawValue: sender.tag)!) {
         case .slow:
             playSound(rate: 0.5)
         case .fast:
@@ -65,12 +64,13 @@ class PlaySoundsViewController: UIViewController {
         case .reverb:
             playSound(reverb: true)
         }
-        
+
         configureUI(.playing)
     }
-    
+
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
         print("Stop Audio Button Pressed")
+        stopAudio()
     }
 
 }
--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -10,68 +10,68 @@ import UIKit
 import AVFoundation
 
 class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
-    
+
     var audioRecorder: AVAudioRecorder!
-    
+
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var stopRecordingButton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI(isRecording: false)
     }
-    
-    func configureUI(isRecording: Bool){
+
+    func configureUI(isRecording: Bool) {
         recordButton.isEnabled = !isRecording
         stopRecordingButton.isEnabled = isRecording
-        if(isRecording){
+        if isRecording {
             recordingLabel.text = "Recording in Progress"
-        }else{
+        } else {
             recordingLabel.text = "Tap to Record"
         }
     }
-    
+
     @IBAction func recordAudio(_ sender: Any) {
         configureUI(isRecording: true)
-        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
         let recordingName = "recordedVoice.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = URL(string: pathArray.joined(separator: "/"))
-        
+
         let session = AVAudioSession.sharedInstance()
-        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
-        
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: AVAudioSessionCategoryOptions.defaultToSpeaker)
+
         try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
         audioRecorder.delegate = self
         audioRecorder.isMeteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
     }
-    
+
     @IBAction func stopRecording(_ sender: Any) {
         configureUI(isRecording: false)
         audioRecorder.stop()
         let audioSession = AVAudioSession.sharedInstance()
         try! audioSession.setActive(false)
     }
-    
+
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder,
                                          successfully flag: Bool) {
         if flag {
             performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
-        }else{
+        } else {
             print("Recording was not succesful")
         }
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "stopRecording"{
+        if segue.identifier == "stopRecording" {
             let playSoundsVC = segue.destination as! PlaySoundsViewController
             let recordedAudioURL = sender as! URL
             playSoundsVC.recordedAudioURL = recordedAudioURL
         }
     }
-    
+
 }
 
